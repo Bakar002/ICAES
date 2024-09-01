6075b2733fc8792591036f03bdb1fdcac5350084
Bakar002 <abubakaramjad78600@gmail.com>
2024-09-01 15:24:14 +0500

    new brochure

--- main.js
+++ main.js
@@ -313,11 +313,11 @@ $("#closebutton").click(function(){
 });
 document.getElementById('myBrochureImg').addEventListener('click', function() {
     // Open the image in a new tab
-    window.open('/Images/Background/Flyer.png', '_blank');
+    window.open('/Images/Background/fly.png', '_blank');
 
     // Create a temporary link element
     var link = document.createElement('a');
-    link.href = '/Images/Background/Flyer.png';
+    link.href = '/Images/Background/fly.png';
     link.download = 'Flyer.png';
 
     // Append the link to the body
